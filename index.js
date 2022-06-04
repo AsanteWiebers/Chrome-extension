1672a9ae51173eaba9eaea6327c96f4e1a38c294
Asante Wiebers <67924684+AsanteWiebers@users.noreply.github.com>
2022-06-04 20:40:40 +0200

    added leadsloop array

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 let saveLead = document.getElementById("input-btn")
-let myLeads = []
+let myLeads = ["www.vi.nl","www.mmafighting","www.youtube"]
 let inputEl = document.getElementById("input-el")
 console.log(saveLead)
 
 saveLead.addEventListener('click', function(){
     myLeads.push(inputEl.value);
     console.log(myLeads);
+
+    for (let i = 0; i < myLeads.length; i++) {
+        console.log(myLeads[i]);
+        
+    }
 })
