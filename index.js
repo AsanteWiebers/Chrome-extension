ca04255ea05f8ebae779c5303f5238e613a6fc92
Asante Wiebers <67924684+AsanteWiebers@users.noreply.github.com>
2022-06-25 14:23:58 +0200

    localstorage function

--- index.js
+++ index.js
@@ -1,36 +1,44 @@
-let saveLead = document.getElementById("input-btn")
 let myLeads = []
+let saveLead = document.getElementById("input-btn")
 let inputEl = document.getElementById("input-el")
 const ulEl = document.getElementById("ul-el")
-let a = document.createElement("a");
-let leadsLinks = document.createTextNode
 
 
+const leadsFromLocalStorage = localStorage.getItem(JSON.parse("myLeads"))
 
+// parse turns string into an array
+
+console.log(leadsFromLocalStorage)
 
 saveLead.addEventListener('click', function(){
     myLeads.push(inputEl.value);
     inputEl.value = "";
 
+    // saving leads array to local storage
+    localStorage.setItem("myLeads", JSON.stringify(myLeads))
+    
+    // fetching the items from local storage
+    console.log(localStorage.getItem("myLeads"))
+
     renderLeads()
 })
 
+
+
 function renderLeads(){
-
     let listItems = ''
-
     for (let i = 0; i < myLeads.length; i++) {
 
-        listItems += `<li>
-            <a href="${myLeads[i]}" target="_blank">
-            ${myLeads[i]}</a> 
-                    </li>`
+        listItems += `
+                <li>
+                    <a href="${myLeads[i]}" target="_blank">
+                         ${myLeads[i]}
+                    </a> 
+                </li>
+                `
         console.log(listItems);
     }
-
     ulEl.innerHTML = listItems
-
-
 }
 
 
