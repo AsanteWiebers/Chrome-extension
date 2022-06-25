ca63990a8d1798b24e4ddb69a80f86d5ff5f5380
Asante Wiebers <67924684+AsanteWiebers@users.noreply.github.com>
2022-06-25 21:04:32 +0200

    save button function

--- index.js
+++ index.js
@@ -3,37 +3,28 @@ const saveLead = document.getElementById("input-btn")
 const inputEl = document.getElementById("input-el")
 const ulEl = document.getElementById("ul-el")
 const deleteEl = document.getElementById("delete-btn")
+const saveEl = document.getElementById("save-btn")
 
-const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
+
+const tabs = [
+    { url: "https://www.linkedin.com/in/per-harald-borgen/"}
+]
+
+saveEl.addEventListener("click", function(){
+    
+    console.log(tabs[0].url)
+})
+
+const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads")) // parse turns string into an array
 
 if (leadsFromLocalStorage){
     myLeads = leadsFromLocalStorage
     renderLeads() 
 }
 
-deleteEl.addEventListener("dblclick", function(){
-    myLeads.localStorage.clear()
-})
-
-// parse turns string into an array
-
-
-console.log(leadsFromLocalStorage)
-
-saveLead.addEventListener('click', function(){
-    myLeads.push(inputEl.value);
-    inputEl.value = "";
-
-    // saving leads array to local storage
-    localStorage.setItem("myLeads", JSON.stringify(myLeads))
-    
-    // fetching the items from local storage
-    console.log(localStorage.getItem("myLeads"))
-
-    renderLeads()
-})
-
-
+// Refector the function so that it takes a parameter, leads, that it uses
+// instead of the global myLeads variable. Remember to update all invocations 
+// of the function as well.
 
 function renderLeads(){
     let listItems = ''
@@ -51,5 +42,24 @@ function renderLeads(){
     ulEl.innerHTML = listItems
 }
 
+deleteEl.addEventListener("dblclick", function(){
+    localStorage.clear() // Clearing local storage
+    myLeads = [] // clearing my leads
+    renderLeads() //clears out dom 
+})
 
+console.log(leadsFromLocalStorage)
+
+saveLead.addEventListener('click', function(){
+    myLeads.push(inputEl.value);
+    inputEl.value = "";
+
+    // saving leads array to local storage
+    localStorage.setItem("myLeads", JSON.stringify(myLeads))
+    
+    // fetching the items from local storage
+    console.log(localStorage.getItem("myLeads"))
+
+    renderLeads()
+})
 
